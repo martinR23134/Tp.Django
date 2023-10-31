2f49883423ca5a0ecf795dcafdeb8b0648d76a3d
Avila Esteban <cqasss@gmail.com>
2023-10-31 17:27:52 -0300

    conexion postgresql

--- grupo13/settings.py
+++ grupo13/settings.py
@@ -74,12 +74,16 @@ WSGI_APPLICATION = 'grupo13.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#      }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tpdjango',
+        'USER': 'admin',  # TU USUARIO
+        'PASSWORD': 'admin',  # TU PASSWORD
+        'HOST': 'localhost',
+        'PORT': ''
+    }
+}
 
 
 # Password validation
